2d0137fe4574573ee5d8f70c9408d449f922ee0c
Jakku Night <J97k107u45k117n@gmail.com>
2023-06-23 22:25:01 -0400

    Adding content and structuring...

---
+++ schema.js
@@ -0,0 +1,49 @@
+let schema = {
+    title: "Curso de Algoritmia y Programación de Bajo Nivel",
+    sections: [
+        {
+            title: "Introducción",
+            subsections: [
+                {
+                    title: "Motivación"
+                },
+                {
+                    title: "El Problema de la enseñanza de la Algoritmia y la Programación"
+                },
+                {
+                    title: "Sistemas de lógica",
+                    topics: [
+                        {
+                            title: "Lógica Inductiva"
+                        },
+                        {
+                            title: "Lógica Deductiva"
+                        },
+                        {
+                            title: "Definiciones"
+                        },
+                        {
+                            title: "Proposiciones",
+                            subtopics: [
+                                {
+                                    title: "Axiomas"
+                                },
+                                {
+                                    title: "Postulados"
+                                },
+                                {
+                                    title: "Teoremas"
+                                },
+                                {
+                                    title: "Recíprocos"
+                                }
+                            ]
+                        },
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+export default schema;
